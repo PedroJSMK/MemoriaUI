ece90099c2bb2b343251fa9fd485f186d5c7a426
Pedro Marques <pedrokanagusto@hotmail.com>
2021-09-12 10:18:27 -0300

    Initial Commit

---
+++ MemoriaUI/MemoriaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriaUIApp.swift
+//  MemoriaUI
+//
+//  Created by Pedro Kanagusto on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoriaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
